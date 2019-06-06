ea430e8e9b4305a8bd9debd57dc2dbcfa707b438
Idan Miara <idanm@omnisys.co.il>
2019-06-06 13:23:52 +0300

    example.py fix

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ from gdalos import GeoRectangle, OvrType
 def test_srtm():
     my_extent = GeoRectangle.from_min_max(5, 85, 30, 40)
     srtm_path = r'd:\maps\srtm.tif'
-    warp_CRS = [None, 32, 34]:
+    warp_CRS = [None, 32, 34]
     gdalos_trans(filename=srtm_path, extent=my_extent, warp_CRS=warp_CRS, dst_nodatavalue=0,
                  hide_nodatavalue=True, ovr_type=OvrType.create_external_auto)
 
