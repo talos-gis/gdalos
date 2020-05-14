c86ed672b419d410ead1510188369bfd1f5567b9
Idan Miara <idan@miara.com>
2020-05-14 19:23:54 +0300

    .

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools
+from setuptools import setup, find_packages
 
 from src.gdalos_data.__data__ import (
     __author__,
@@ -8,7 +8,7 @@ from src.gdalos_data.__data__ import (
     __version__,
 )
 
-setuptools.setup(
+setup(
     name="gdalos",
     version=__version__,
     author=__author__,
@@ -16,8 +16,9 @@ setuptools.setup(
     license=__license__,
     url=__url__,
     description="a simple gdal translate/warp/addo python wrapper for raster batch processing",
-    package_dir={"": "src"},
-    packages=["gdalos", "gdalos_data", "gdalos_qt"],
+    # packages=["gdalos", "gdalos_data", "gdalos_qt"],
+    packages=find_packages("src"),  # include all packages under src
+    package_dir={"": "src"},   # tell distutils packages are under src
     install_requires=["gdal"],
     extras_require={"PyQt": ["fidget", "PyQt5"], "PySide": ["fidget", "PySide2"]},
     python_requires=">=3.6.0",
