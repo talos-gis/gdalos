b47238065271adf5a318cfb35c7e4fef7a2de261
Idan Miara <idan@miara.com>
2020-09-16 15:42:08 +0300

    version infomation updated

--- src/gdalos/__init__.py
+++ src/gdalos/__init__.py
@@ -1,4 +1,5 @@
+from gdalos_data.__data__ import __version__
 from gdalos.gdalos_main import OvrType, RasterKind, gdalos_trans
 from gdalos.rectangle import GeoRectangle
 
-__all__ = [GeoRectangle, OvrType, RasterKind, gdalos_trans]
+__all__ = [GeoRectangle, OvrType, RasterKind, gdalos_trans, __version__]
--- src/gdalos_data/__data__.py
+++ src/gdalos_data/__data__.py
@@ -1,4 +1,4 @@
-__version__ = "0.45.0"
+__version__ = "0.45.1"
 __author__ = "Idan Miara, Ben Avrahami"
 __author_email__ = "idan@miara.com"
 __license__ = "MIT"
