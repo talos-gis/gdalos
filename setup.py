9e9048fb128d63393fbb102a706308c7c75fdf39
Idan Miara <idan@miara.com>
2021-01-20 16:44:29 +0200

    setup.py fix

--- setup.py
+++ setup.py
@@ -1,24 +1,45 @@
 from setuptools import setup, find_packages
 
 from src.gdalos import (
-    __name__,
+    __pacakge_name__,
     __author__,
     __author_email__,
     __license__,
     __url__,
     __version__,
+    __description__,
 )
 
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Topic :: Scientific/Engineering :: GIS',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+]
+
+__readme__ = open('README.rst', encoding="utf-8").read()
+__readme_type__ = 'text/x-rst'
+
+package_root = 'src'   # package sources are under this dir
+packages = find_packages(package_root)  # include all packages under package_root
+package_dir = {'': package_root}  # packages sources are under package_root
+
 setup(
-    name=__name__,
+    name=__pacakge_name__,
     version=__version__,
     author=__author__,
     author_email=__author_email__,
     license=__license__,
     url=__url__,
-    description="a simple gdal translate/warp/addo python wrapper for raster batch processing",
-    packages=find_packages("src"),  # include all packages under src
-    package_dir={"": "src"},   # tell distutils packages are under src
+    long_description=__readme__,
+    long_description_content_type=__readme_type__,
+    description=__description__,
+    packages=packages,
+    package_dir=package_dir,
     extras_require={"gdal": ["gdal"], "PyQt": ["fidget", "PyQt5"], "PySide": ["fidget", "PySide2"]},
     python_requires=">=3.6.0",
     include_package_data=True,
--- src/gdalos/__init__.py
+++ src/gdalos/__init__.py
@@ -1,6 +1,7 @@
-__name__ = "gdalos"
+__pacakge_name__ = "gdalos"
 __version__ = "0.48.0"
 __author__ = "Idan Miara, Ben Avrahami"
 __author_email__ = "idan@miara.com"
 __license__ = "MIT"
 __url__ = r"https://github.com/talos-gis/gdalos"
+__description__ = "a simple gdal translate/warp/addo python wrapper for raster batch processing"
