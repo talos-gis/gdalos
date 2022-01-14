82b307493d927ba088552fefbcabc663267b2651
Idan Miara <idan@miara.com>
2022-01-14 16:39:08 +0200

    v0.64.0 - restore compatibility with gdal<3 (set_traditional_gis_order)

--- src/gdalos/__init__.py
+++ src/gdalos/__init__.py
@@ -1,8 +1,20 @@
-from osgeo.osr import OAMS_TRADITIONAL_GIS_ORDER
-from osgeo_utils.auxiliary.osr_util import set_default_axis_order
-set_default_axis_order(OAMS_TRADITIONAL_GIS_ORDER)
+from typing import Tuple
 
-version = (0, 63, 0)
+import osgeo
+
+
+def version_tuple(version: str) -> Tuple[int]:
+    return tuple(int(s) for s in str(version).split('.') if s.isdigit())[:3]
+
+
+def set_traditional_gis_order():
+    if gdal_version >= (3, 0):
+        from osgeo.osr import OAMS_TRADITIONAL_GIS_ORDER
+        from osgeo_utils.auxiliary.osr_util import set_default_axis_order
+        set_default_axis_order(OAMS_TRADITIONAL_GIS_ORDER)
+
+
+version = (0, 64, 0)
 
 __package_name__ = "gdalos"
 __version__ = '.'.join(str(v) for v in version)
@@ -11,3 +23,8 @@ __author_email__ = "idan@miara.com"
 __license__ = "MIT"
 __url__ = r"https://github.com/talos-gis/gdalos"
 __description__ = "a simple gdal translate/warp/addo python wrapper for raster batch processing"
+
+gdalos_version = version
+gdal_version = version_tuple(osgeo.__version__)
+
+set_traditional_gis_order()
--- src/gdalos/gdalos_versions.py
+++ src/gdalos/gdalos_versions.py
@@ -1,14 +1,5 @@
-from typing import Tuple
+from gdalos import gdal_version, version_tuple, gdalos_version
 
-import osgeo
-from gdalos import version as gdalos_version  # noqa
-
-
-def version_tuple(version: str) -> Tuple[int]:
-    return tuple(int(s) for s in str(version).split('.') if s.isdigit())[:3]
-
-
-gdal_version = version_tuple(osgeo.__version__)
 gdal_cog_support = gdal_version >= (3, 1)
 gdal_multi_thread_support = gdal_version >= (3, 2)
 gdal_workaround_warp_scale_bug = gdal_version < (3, 3)  # workaround https://github.com/OSGeo/gdal/issues/3232
